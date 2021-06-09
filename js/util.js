ca36afd14b5d99e0d56ce794845e1b6437f6bc81
kalaganov5 <kalaganov5@gmail.com>
2021-06-09 10:04:40 +0300

    getRandomNumber доработка, теперь можно передать цифры в любом порядке или передать отрицательное цисло, которое будет переведено в положительное. Нельзя только передавать равные числа

--- js/util.js
+++ js/util.js
@@ -5,11 +5,13 @@
  * Формула взята https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
  */
 const getRandomNumber = function (min, max) {
-  if (min < 0 || min > max || min === max) {
+  if (min === max) {
     return Error('Неверное значение');
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  // большее и меньшее вычислим с помощью Math.min и Math.max, чтобы пользователю не запоминать очередность
+  // с помощью Math.abs передем отризацтельное число в положительное
+  min = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  max = Math.floor((Math.max(Math.abs(min), Math.abs(max))));
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
