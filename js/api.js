d0f2ca12c07a5c545c8695a6fd9744b359aa1c54
kalaganov5 <kalaganov5@gmail.com>
2021-07-21 12:06:55 +0300

    Загрузка данных с сервера, убраны демо данные

--- js/api.js
+++ js/api.js
@@ -1,17 +1,30 @@
-const createFetch = (onSuccess, onError) => {
-  fetch('https://23.javascript.pages.academy/kekstagram/data')
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`${response.status} ${response.statusText}`);
-    })
-    .then((json) => {
-      onSuccess(json);
-    })
-    .catch((err) => {
-      onError(err);
-    });
-};
+import {renderThumbnails} from './draw-thumbnails.js';
+// const createFetch = (onSuccess, onError) => {
+//   fetch('https://23.javascript.pages.academy/kekstagram/data')
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new Error(`${response.status} ${response.statusText}`);
+//     })
+//     .then((json) => {
+//       onSuccess(json);
+//     })
+//     .catch((err) => {
+//       onError(err);
+//     });
+// };
 
-export {createFetch};
+
+// берем данные с сервера
+fetch('https://23.javascript.pages.academy/kekstagram/data')
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    // Здесь должно быть функция показ ошибки при загрузки данных
+    throw new Error(`${response.status} ${response.statusText}`);
+  })
+  .then((userPhotos) => {
+    renderThumbnails(userPhotos);
+  });
--- js/draw-thumbnails.js
+++ js/draw-thumbnails.js
@@ -1,29 +1,18 @@
-import {createPhotoDescriptions} from './mock/demo-data.js';
-// import {createFetch} from './api.js';
 import {drawBigPicture, modalBigPicture} from './draw-big-picture.js';
 import {modalOpen, whatModalOpen} from './modal.js';
-
-// const renderUserPhotos = createFetch(
-//   (photos) => {
-//     console.log(photos);
-//   },
-//   (err) => {
-//     console.log(err);
-//   },
-// );
 
 /**
  * Рендер разметки миниатюр
  * @param {number} quantity - число миниатюр для генерации
  */
-const renderThumbnails = (quantity) => {
+const renderThumbnails = (array) => {
   const thumbnailsBlock = document.querySelector('.pictures');
   // @ts-ignore
   const thumbnailTemplate = document.querySelector('#picture').content;
   const fragmentThumbnail = document.createDocumentFragment();
 
   // Создаём миниатюры и ставим в раметку
-  createPhotoDescriptions(quantity).forEach(({url, likes, comments, description}) => {
+  array.forEach(({url, likes, comments, description}) => {
     const thumbnailItem = thumbnailTemplate.cloneNode(true);
 
     thumbnailItem.querySelector('.picture__img').src = url;
